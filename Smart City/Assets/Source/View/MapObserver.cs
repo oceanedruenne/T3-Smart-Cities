f294f23964a9c42f1a9f4a631aebdec61b4ee215
Danae ALBRECHT-MARTIN <danae.albrecht-martin@etu.unistra.fr>
2022-12-07 11:39:56 +0100

    Modification de la scene : Ajout des infos de la tile sélectionner et création de la fonction d'affichage

--- Smart City/Assets/Source/View/MapObserver.cs
+++ Smart City/Assets/Source/View/MapObserver.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 using Source.Model;
 using Source.View;
 using JeuScript;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
 public class MapObserver : ScriptableObject, Observer
 {
     GameObject gridObject = GameObject.Find("Grid");
@@ -12,6 +17,14 @@ public class MapObserver : ScriptableObject, Observer
     int size;
     GameObject tilePrefab;
     private Dictionary<Vector3Int, GameObject> tilesd;
+
+    //Panel contenant les infos de notre case sélectionnée
+    private GameObject panelInfos = GameObject.Find("Panel Infos");
+    //Infos dans le panel
+    private TextMeshProUGUI textLevel = GameObject.Find("Level").GetComponent<TextMeshProUGUI>();
+    private Image imageObject = GameObject.Find("Sprite").GetComponent<Image>();
+    private TextMeshProUGUI textPrice = GameObject.Find("Price").GetComponent<TextMeshProUGUI>();
+    private TextMeshProUGUI textIncome = GameObject.Find("Income").GetComponent<TextMeshProUGUI>();
 
     public MapObserver(Map map){
         genMap(map);
@@ -72,5 +85,20 @@ public class MapObserver : ScriptableObject, Observer
     public void reactToPos(Map map, uint posx, uint posy){
         Building building = map.buildings[posx,posy];
         getTileAtPosition(new Vector3Int((int)posx,(int)posy,0)).GetComponent<Tile>().changeSprite(building.type, building.level);
+    }
+
+    public void changeInfos(int level, int price, int income, Sprite image)
+    {
+        //Niveau du bâtiment sélectionner
+        textLevel.text = "Niveau " + level.ToString();
+
+        //Apparence
+        imageObject.sprite = image;
+
+        //Prix et revenu
+        textPrice.text = "Prix : " + price.ToString();
+        textIncome.text = "Gain : " + income.ToString();
+
+        panelInfos.SetActive(true);
     }
 }
